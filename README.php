ff75ddc950c10d0598d699077e07f81bd94211da
milad <35831618+lithiumx57@users.noreply.github.com>
2020-05-27 16:01:04 +0430

    Update README.php

--- README.php
+++ README.php
@@ -1,19 +1,7 @@
-To use, you must first create a Laravel project
-Then enter the app folder through the command cd app
-Then run the command  git clone https://github.com/lithiumx57/Panel.git  to download the information
-And finally in the file web.php run the function AdminDynamicModels::initialize();
+<p>create laravel project and go to the app folder by cd app</p> 
 
-  protected function fields(): array
-  {
-    return [
-      xStringField("name", xLabel("نام دسته بندی"), xShowInAdmin()),
-      xBoolField("approved", xLabel("فعال"), xDefault(true)),
-      xForeignField("parent", Category::class, "parentCategory", ['name'], null,
-        xLabel("دسته پدر"),
-        xConditions(['parent' => 0]),
-        xDefaultSelectItems([0 => "دسته اصلی"])
-      )
-    ];
-  }
+<p>for install :</p>
 
-composer require lithiumx57/xpanel
+<blockquote>
+    git clone https://github.com/lithiumx57/Panel.git
+</blockquote>
